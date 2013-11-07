a9b86c674f8924554f991d9985cc4ec82df7de65
Patrick Widener <patrick.widener@sandia.gov>
2013-11-07 14:27:28 -0700

    added some message types.  still have to compile all this eventually

--- src/formats.cc
+++ src/formats.cc
@@ -76,6 +76,64 @@ namespace drift {
       FMStructDescRec_list_terminator
     };
 
+  FMField simple_part_field_list[] = 
+    {
+      {"payload", "string", sizeof(char*), FMOffset(simple_part_ptr, payload)},
+      {"store_type", "integer", sizeof(unsigned long), FMOffset(simple_part_ptr, store_type)},
+      {"metadata", "string", sizeof(char*), FMOffset(simple_part_ptr, metadata)}
+      FMField_list_terminator
+    };
+  FMStructDescRec simple_part_format_list[] =
+    {
+      {"simple_part", simple_part_field_list, sizeof(simple_part), (FMOptInfo*)0 },
+      FMStructDescRec_list_terminator
+    };
+
+  FMField complex_part_field_list[] = 
+    {
+      {"count", "integer", sizeof(unsigned long), FMOffset(complex_part_ptr, count)},
+      {"parts", "simple_part[count]", sizeof(simple_part), FMOffset(complex_part_ptr, parts)},
+      {"metadata", "string", sizeof(char*), FMOffset(complex_part_ptr, metadata)},
+      FMField_list_terminator
+    };
+  FMStructDescRec complex_part_format_list[] = 
+    {
+      {"complex_part", complex_part_field_list, sizeof(complex_part), (FMOptInfo*)0 },
+      {"simple_part", simple_part_field_list, sizeof(simple_part), (FMOptInfo*)0 },
+      FMStructDescRec_list_terminator
+    };
+
+  FMField simple_part_xfer_field_list[] =
+    {
+      {"flags", "integer", sizeof(unsigned long), FMOffset(simple_part_xfer_ptr, flags)},
+      {"part", "simple_part", sizeof(simple_part), FMOffset(simple_part_xfer_ptr, part)},
+      {"index_name", "string", sizeof(char*), FMOffset(simple_part_xfer_ptr, index_name)},
+      {"index_spec", "string", sizeof(char*), FMOffset(simple_part_xfer_ptr, index_spec)},
+      FMField_list_terminator
+    };
+  FMStructDescRec simple_part_xfer_format_list[] =
+    {
+      {"simple_part_xfer", simple_part_xfer_field_list, sizeof(simple_part_xfer), (FMOptInfo*)0 },
+      {"simple_part", simple_part_field_list, sizeof(simple_part), (FMOptInfo*)0 },
+      FMStructDescRec_list_terminator
+    };
+
+  FMField complex_part_xfer_field_list[] = 
+    {
+      {"flags", "integer", sizeof(unsigned long), FMOffset(complex_part_xfer_ptr, flags) },
+      {"part", "complex_part", sizeof(complex_part), FMOffset(complex_part_xfer_ptr, part) },
+      {"index_name", "string", sizeof(char*), FMOffset(complex_part_xfer_ptr, index_name)},
+      {"index_spec", "string", sizeof(char*), FMOffset(complex_part_xfer_ptr, index_spec)},
+      FMField_list_terminator
+    };
+  FMStructDescRec complex_part_xfer_format_list[] =
+    {
+      {"complex_part_xfer", complex_part_xfer_field_list, sizeof(complex_part_xfer), (FMOptInfo*)0 },
+      {"complex_part", complex_part_field_list, sizeof(complex_part), (FMOptInfo*)0 },
+      FMStructDescRec_list_terminator
+    };
+      
+
 
 }
 
--- src/formats.h
+++ src/formats.h
@@ -47,6 +47,45 @@ namespace drift {
   extern FMField put_immediate_json_field_list[];
   extern FMStructDescRec put_immediate_json_format_list[];
 
+  typedef struct _simple_part {
+    char *payload;
+    unsigned long store_type;
+    char *metadata;
+  } simple_part, *simple_part_ptr;
+
+  extern FMField simple_part_field_list[];
+  extern FMStructDescRec simple_part_format_list[];
+
+  typedef struct _complex_part {
+    unsigned long count;
+    struct _simple_part *parts;
+    char *metadata;
+  } complex_part, *complex_part_ptr;
+  
+  extern FMField complex_part_field_list[];
+  extern FMStructDescRec complex_part_format_list[];
+
+  typedef struct _simple_part_xfer {
+    unsigned long flags;
+    struct _simple_part part;
+    char* index_name;
+    char* index_spec;
+  } simple_part_xfer, *simple_part_xfer_ptr;
+
+  extern FMField simple_part_xfer_field_list[];
+  extern FMStructDescRec simple_part_xfer_format_list[];
+
+  typedef struct _complex_part_xfer {
+    unsigned long flags;
+    struct _complex_part part;
+    char *index_name;
+    char *index_spec;
+  } complex_part_xfer, *complex_part_xfer_ptr;
+
+  extern FMField complex_part_xfer_field_list[];
+  extern FMStructDescRec complex_part_xfer_format_list[];
+
+
 }
 
 
