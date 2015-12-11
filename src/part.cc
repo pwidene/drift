a2c4d4f75411ed5a4bb425185cb2f592cd59f5db
Patrick Widener <patrick.widener@sandia.gov>
2015-12-11 15:40:17 -0700

    bleh

--- src/part.cc
+++ src/part.cc
@@ -57,60 +57,23 @@ part::store()
      * do the redox async store
      */
     try {
-      auto& mtime = mtime_; // local ref for lambda capture
+      rdx_.command<string>( {"SET", tag_.to_string() + ':' + "immediate", immediate_.asString() },
+			    [](redox::Command<string>& c) {
+			      if (!c.ok()) throw c;
+			    });
+
+      std::ostringstream ostr;
+      ostr << mtime_ = boost::chrono::system_clock::now();
+      rdx_.command<string>( { "SET", tag_.to_string() + ':' + "mtime", ostr.str() },
+			    [](redox::Command<string>& c) {
+			      if (!c.ok()) throw c;
+			    });
       
-      rdx_.command<string>({"SET", tag_.to_string() + ':' + "immediate", immediate_.asString(),},
-			   [mtime](redox::Command<string>& c) {
-			     if (!c.ok()) throw c;
-			     mtime = 
-			   });
     }
     catch (redox::Command<string>& cex) {
       // right now do nothing
     }
-					    
 
-  //  An empty node_uri means we haven't been stored yet
-  bool creating;
-  if (node_uri_.empty()) {
-    creating = true;
-    req.set_method ( web::http::methods::POST );
-  } else {
-    creating = false;
-    req.set_method ( web::http::methods::PUT );
-  }
-
-  req.headers().add ( "Accept", "application/json" );
-  req.headers().add ( "Content-Type", "application/json" );
-
-  web::json::value props;
-  json_props ( props );
-    
-  req.set_body ( props );
-
-  pplx::task<void> ptask = 
-    client.request(req).then([](web::http::http_response response) -> pplx::task<json::value> {
-	if ( response.status_code() == web::http::status_codes::Created and creating) {
-
-	  // get the URI from the REST response and save it
-	  web::http::http_headers::iterator i = response.find("Location");
-	  if (i != response.end()) {
-	    node_uri_ = i->second;
-	  }
-
-	  ctime_ = mtime_ = boost::chrono::system_clock::now();
-
-	} else if ( response.status_code() == web::http::status_codes::NoContent and not creating ) {
-	  
-	  // this was a triumph, I'm making a note here, HUGE SUCCESS
-	  mtime_ = boost::chrono::system_clock::now();
-
-	} else {
-	  
-	  // We're in uncharted territory, pull the ripcord
-	  throw response;
-	}
-      });
 }
 
 
@@ -118,38 +81,21 @@ part::store()
 void
 part::load()
 {
-  std::ostringstream ostr;
-  ostr << part::get_n4j_rest_uri() << node_id_;
-  web::http::client::http_client cli ( ostr.str() );
-  web::http::http_request req ( web::http::methods::GET );
-  
-  req.headers().add ( "Accept", "application/json" );
-  
-  pplx::task<void> ptask = 
-  client.request(req).then([](web::http::http_response response) -> pplx::task<json::value> {
-      if ( response.status_code() == web::http::status_codes::OK ) {
-	  return response.extract_json();
-	}
-	return pplx::task_from_result(json::value());
-    });
-    /*
-      .then([](pplx::task<json::value> previousTask) {
-	  try {
-	    const json::value& v = previousTask.get();
-	    for (auto iter = v.cbegin(); iter != v.cend(); ++iter) {
-	      const json::value &str = iter->first;
-	      const json::value &val = iter->second;
-	      std::cout << "key: " << str.as_string() << ", value = " << val.to_string() << std::endl;
-	    }
-	  }
-	  catch (const http_exception& e) {
-	    std::ostringstream ss;
-	    ss << e.what() << endl;
-	    std::cout << ss.str();
-	  }
-	}
-	);
-    */
+
+  try {
+    rdx_.command<string>( {"GET", tag_.to_string() + ':' + "immediate" },
+			  [](redox::Command<string>& c) {
+			    if (!c.ok()) throw c;
+			  });
+
+    // assign c.reply() to the immediate_ member
+    // throw should protect us against errors
+    // what to do about state mask?
+
+  }
+  catch (redox::Command<string>& cex) {
+    // again right now do nothing
+  }
 }
 
 
